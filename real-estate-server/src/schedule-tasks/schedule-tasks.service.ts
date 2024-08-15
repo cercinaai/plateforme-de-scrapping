f35bd63a6088f17431ec7306f8c869c411b18c02
Haythem Ben Jbara <haythembenjbara@gmail.com>
2024-08-15 13:17:40 +0100

    INSERTING NEW SSH LOGIC + ADDING CRON JOBS

--- real-estate-server/src/schedule-tasks/schedule-tasks.service.ts
+++ real-estate-server/src/schedule-tasks/schedule-tasks.service.ts
@@ -22,10 +22,10 @@ export class ScheduleTasksService implements OnModuleInit {
         // heath_check_job.start();
     }
 
-    @Cron(CronExpression.EVERY_12_HOURS, { disabled: true })
+    @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
     async startPeriodicCrawlers() {
         await this.crawlerService.populate_database();
-        const heath_check_job = new CronJob(CronExpression.EVERY_10_SECONDS, async () => {
+        const heath_check_job = new CronJob(CronExpression.EVERY_10_MINUTES, async () => {
             const session_saved = await this.crawlerService.heathCheck();
             if (!session_saved) return;
             heath_check_job.stop();
