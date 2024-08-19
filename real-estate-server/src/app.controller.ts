65ad24679e54d71a6a8d007fa4fc3d9dead43db3
Haythem Ben Jbara <haythembenjbara@gmail.com>
2024-08-19 10:39:21 +0100

    ADD A ENDPOINT TO TEST CRAWLER + FIX ENTRYPOINT DIR

--- real-estate-server/src/app.controller.ts
+++ real-estate-server/src/app.controller.ts
@@ -1,5 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ScheduleTasksService } from './schedule-tasks/schedule-tasks.service';
+import { Throttle } from '@nestjs/throttler';
+import { RealEstateAuthGuard } from './auth/guard/RealEstate.guard';
 
 @Controller()
-export class AppController { }
+export class AppController {
+    constructor(private readonly scheduleService: ScheduleTasksService) { }
+
+    @Throttle({ short: { limit: 2, ttl: 1000 }, long: { limit: 5, ttl: 60000 } })
+    @UseGuards(RealEstateAuthGuard)
+    @HttpCode(HttpStatus.ACCEPTED)
+    @Get('populate_database')
+    async populate_database() {
+        await this.scheduleService.start_crawler_with_health_check();
+        return "Database Started Populating..."
+    }
+}
