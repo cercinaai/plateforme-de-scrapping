a2891bab330548d9febaba67cd241a17041e5ddb
Haythem Ben Jbara <haythembenjbara@gmail.com>
2024-09-16 22:33:15 +0100

    REVERT TO ALL CRAWLERS

--- real-estate-server/src/crawler/crawler.service.ts
+++ real-estate-server/src/crawler/crawler.service.ts
@@ -13,8 +13,8 @@ export class CrawlerService {
 
     async populate_database() {
         this.logger.log('Populating Crawler Queues...');
-        // await this.addJobAndWaitForCompletion('boncoin-crawler');
-        // await this.addJobAndWaitForCompletion('seloger-crawler');
+        await this.addJobAndWaitForCompletion('boncoin-crawler');
+        await this.addJobAndWaitForCompletion('seloger-crawler');
         await this.addJobAndWaitForCompletion('logicimmo-crawler');
         await this.addJobAndWaitForCompletion('bienici-crawler');
         this.logger.log('Crawler Queues Populated');
