2e65c93f3c7dfc369bb8cfee87a95aaa580a4bea
Haythem Ben Jbara <haythembenjbara@gmail.com>
2024-09-16 13:59:25 +0100

    ADD A SECURITY LAYER ON PRE NAVIGATION HOOKS TO REMOVE INSTANT CLOSING OF PROCESS

--- real-estate-server/src/crawler/boncoin/preNavigation/preHooks.register.ts
+++ real-estate-server/src/crawler/boncoin/preNavigation/preHooks.register.ts
@@ -2,18 +2,22 @@ import { PlaywrightCrawlingContext, PlaywrightHook } from "crawlee";
 
 
 const interceptBoncoinHttpResponse = async (context: PlaywrightCrawlingContext) => {
-    const { page } = context;
-    page.on('response', async (response) => {
-        const url = response.url();
-        if (url.includes('https://api.leboncoin.fr/finder/search')) {
-            const body = await response.json();
-            let ads = body['ads'];
-            if (!ads) return;
-            await page.evaluate((transformed_ads) => {
-                window['crawled_ads'] = transformed_ads;
-            }, ads);
-        }
-    })
+    try {
+        const { page } = context;
+        page.on('response', async (response) => {
+            const url = response.url();
+            if (url.includes('https://api.leboncoin.fr/finder/search')) {
+                const body = await response.json();
+                let ads = body['ads'];
+                if (!ads) return;
+                await page.evaluate((transformed_ads) => {
+                    window['crawled_ads'] = transformed_ads;
+                }, ads);
+            }
+        })
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 
