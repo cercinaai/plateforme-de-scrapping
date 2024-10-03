bae3c680aa83a1b4e58b5ab1f16e3790e4febe7a
Haythem Ben Jbara <haythembenjbara@gmail.com>
2024-10-03 22:50:56 +0100

    FIX TYPO IN FILE INGESTION

--- real-estate-crawlers/src/data-ingestion/file.ingestion.ts
+++ real-estate-crawlers/src/data-ingestion/file.ingestion.ts
@@ -35,7 +35,7 @@ export const uploadFileIntoBucket = async (file: string, target: string): Promis
             Body: fileBuffer
         });
         await s3.send(command);
-        return `https://f003.backblazeb2.com/file/${process.env.AWS_S3_BUCKET_NAME})}/${targetName}`;
+        return `https://f003.backblazeb2.com/file/${process.env.AWS_S3_BUCKET_NAME}/${targetName}`;
     } catch (error) {
         if (error.message.includes('ServiceUnavailable: no tomes available')) {
             s3 = configure_bucket();
@@ -56,7 +56,7 @@ export const uploadBufferIntoBucket = async (buffer: Buffer, fileName: string, t
             Body: buffer
         });
         await s3.send(command);
-        return `https://f003.backblazeb2.com/file/${process.env.AWS_S3_BUCKET_NAME})}/${targetName}`;
+        return `https://f003.backblazeb2.com/file/${process.env.AWS_S3_BUCKET_NAME}/${targetName}`;
     } catch (error) {
         if (error.message.includes('ServiceUnavailable: no tomes available')) {
             s3 = configure_bucket();
